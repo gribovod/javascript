08dfb1caf3f95c8a7aa3842d292f2bb80b8ad0bc
Vadim <gribovod>
2015-08-17 23:37:31 +0300

    Реализована функция покупки. Нужно писать функцию добавления

--- 9-8-2015/js.js
+++ 9-8-2015/js.js
@@ -5,7 +5,7 @@ var MesagaShow = true;
 
 function startGame()
 {
-    document.getElementById("start_button").style.visibility = "hidden";
+    document.getElementById("start_button").style.visibility = "hidden"; //style.display=='none'
     setInterval("goldPlus()", 500);
 }
 function goldPlus()
@@ -25,12 +25,48 @@ function goldPlus()
         dGold = 0;
     }
     gold += dGold;
-
-
     document.getElementById("gold_box").style.width = gold + "px";
+    document.getElementById("gold_text").innerHTML = "Сейчас в закромах " + gold + " золота";
+
     console.log("Текущее значение gold - " + gold);
 }
 function pay()
 {
-    gold -= 50;
+    var ans = prompt("Выберите какой источник ресурса хотите купить: \n\n1. Дерево - 200 золота\n2. Камень - 500 золота\n3. Песок - 350 золота\n\n и введите цифру:");
+    switch (parseInt(ans))
+    {
+        case 1:
+            if(gold > 200)
+            {
+                gold -= 200;
+            }
+            else
+            {
+                alert("Мало золота!")
+            }
+            break;
+        case 2:
+            if(gold > 500)
+            {
+                gold -= 500;
+            }
+            else
+            {
+                alert("Мало золота!")
+            }
+            break;
+        case 3:
+            if(gold > 350)
+            {
+                gold -= 350;
+            }
+            else
+            {
+                alert("Мало золота!")
+            }
+            break;
+        default:
+            alert("Я не понял что Вы ввели! Просто введите цифру и все!!!")
+    }
+
 }
