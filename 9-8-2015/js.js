73193a0ac7d640fc69fc671eaa0f366f7ca75683
Vadim <gribovod>
2015-08-17 20:38:29 +0300

    Зарработало изменение голда

--- 9-8-2015/js.js
+++ 9-8-2015/js.js
@@ -1,1 +1,21 @@
-document.write('Это скрипт');
+var gold = 0;
+function startGame()
+{
+    var goldTimer=setInterval("goldPlus()", 500);
+}
+function goldPlus()
+{
+    gold+=1;
+    console.log("Текущее значение gold - " + gold);
+    var div = document.getElementById("gold_box");
+    div.style.width=gold+"px";
+    var a = div.style.width;
+
+    console.log("Значение a - " + a);
+/*
+    var a = document.getElementById('gold').style.width;
+    a.toString();
+
+    document.getElementById('goldbox').offsetWidth = gold;
+*/
+}
