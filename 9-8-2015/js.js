ca03876a7650b28c596fac9e35169bbb7b1ae0a1
Vadim <gribovod>
2015-08-22 00:07:27 +0300

    Ростут фермы и добавляются! Код не подчищен

--- 9-8-2015/js.js
+++ 9-8-2015/js.js
@@ -1,4 +1,4 @@
-var gold = 950;
+var gold = 1950;
 var dGold = 1;
 var maxGold = 970;
 var MesagaShow = true;
@@ -8,6 +8,7 @@ function startGame()
     document.getElementById("start_button").style.visibility = "hidden"; //style.display=='none'
     setInterval("goldPlus()", 500);
 }
+
 function goldPlus()
 {
     if (gold < maxGold)
@@ -28,7 +29,7 @@ function goldPlus()
     document.getElementById("gold_box").style.width = gold + "px";
     document.getElementById("gold_text").innerHTML = "Сейчас в закромах " + gold + " золота";
 
-    console.log("Текущее значение gold - " + gold);
+   // console.log("Текущее значение gold - " + gold);
 }
 function pay()
 {
@@ -74,18 +75,98 @@ function pay()
 var derevo = 0;
 var derevoArr = [];
 var SummaFermDereva = 0;
+//var derevoHTML = "";
+//var currentID="";
 function derevoAdd()
 {
-    if (SummaFermDereva == 0) {
-        document.getElementById("derevo").innerHTML = "Всего дерева 0 \n <div id=\"d0\">70</div>";
-        SummaFermDereva++;
-    }
-    else
-    {
-        document.getElementById("derevo").innerHTML = "Всего дерева 0 \n <div id=\"d0\">70</div> <div id=\"d1\">10</div>";
-    }
+    /* При таком способе добавления потм нельзя обратится к созданому
+
+    document.getElementById("derevo").innerHTML= derevoHTML + "<div id=\"d"+SummaFermDereva+"\"" +">0<\/div>";
+    currentID = "d" + SummaFermDereva;
+    document.getElementById(currentID).style.left = ((SummaFermDereva * 50) + 5) + "px";
+    derevoArr[SummaFermDereva] = currentID;
+    SummaFermDereva++;
+    derevoHTML = document.getElementById("derevo").innerHTML;
+    */
+
+    // пробуем через create
+    var curID = "d" + SummaFermDereva;
+    var curCSS = "left: " + ((SummaFermDereva * 50) + 5) + "px; height: 10px";
+    var parElement =  document.getElementById("derevo");
+    var newDiv = create( "div", { id: curID, style: curCSS});
+    parElement.appendChild(newDiv);
+    derevoArr[SummaFermDereva] = curID;
+    SummaFermDereva++;
+
 
 }
+//setInterval("StartFerm()", 2000);
+function StartFerm()
+{
 
-/* CSS:"#d" + (SummaFermDereva + 1) + " {left:5px;",*/
+    for (var i in derevoArr)
+    {
+        console.log("derevoArr - " + derevoArr[i]);
+        var a = parseInt(document.getElementById(derevoArr[i]).style.height);
+        a++;
+        document.getElementById(derevoArr[i]).style.height = a+"px"
+        console.log("a =" + a);
+    }
 
+/*
+     var j = 0;
+
+    for (var i in derevoArr)
+    {
+
+        Высота елемента с ид = " + derevoArr[i] + " (" + a + ")");
+    }
+*/
+}
+
+
+/*
+function startFerm()
+{
+
+
+ currentHeight = document.getElementById(derevoArr[i]).style.height;
+ currentHeight++;
+
+ document.getElementById(derevoArr[i]).style.height = currentHeight;
+
+
+
+ console.log(derevoArr[j]);
+ j++
+ console.log("Изменяем ИД"+ i);
+
+
+ console.log("Добавляем ферму..." + chtoto);
+
+
+
+}
+*/
+// http://ahinea.com/2006/04/14/javascript-dom-create
+function create( name, attributes ) {
+    var el = document.createElement( name );
+    if ( typeof attributes == 'object' ) {
+        for ( var i in attributes ) {
+            el.setAttribute( i, attributes[i] );
+
+            if ( i.toLowerCase() == 'class' ) {
+                el.className = attributes[i];  // for IE compatibility
+
+            } else if ( i.toLowerCase() == 'style' ) {
+                el.style.cssText = attributes[i]; // for IE compatibility
+            }
+        }
+    }
+    for ( var i = 2; i<arguments.length; i++ ) {
+        var val = arguments[i];
+        if ( typeof val == 'string' ) { val = document.createTextNode( val ) };
+        el.appendChild( val );
+    }
+    return el;
+}
